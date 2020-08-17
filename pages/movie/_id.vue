bbd457f85bd5ff67efb7ef8b26478deabed01ade
marvelbark2 <53633390+marvelbark2@users.noreply.github.com>
2020-08-17 04:13:39 +0100

    Update _id.vue

--- pages/movie/_id.vue
+++ pages/movie/_id.vue
@@ -100,7 +100,7 @@ export default {
       menu: [],
       activeMenu: 'overview',
       recommended: null,
-      myvideo: [{name: 'Myvideo', url: 'https://historical-deciduous-antimatter.glitch.me/stream/339BE5B6F215AB211226368305839D804C102A59', duration: 200, type: 'stream'}],
+      myvideo: [{id:1, name: 'Myvideo', url: 'https://historical-deciduous-antimatter.glitch.me/stream/339BE5B6F215AB211226368305839D804C102A59',thumb: 'https://image.tmdb.org/t/p/w370_and_h556_bestv2/5MSDwUcqnGodFTvtlLiLKK0XKS.jpg', duration: 200, type: 'stream'}],
     };
   },
 
